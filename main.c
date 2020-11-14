e15b221f1c63f843859c5b754d0dd6e9c3af9788
Souligna Savann <sousavann@gmail.com>
2020-11-13 19:51:03 -0500

    ajout du fichier .gitignore

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>	// 'stdio' permet d'utiliser la fonction 'printf'
+#include <stdlib.h>	// 'stdlib' permet de gérer la mémoire pour cette fonction
+
+
+
+//'main' c'est la fonction principale d'un programme écrit en C
+int main()
+{
+    //pour afficher le message
+    printf("Hello World!\n");	//le 'n' est un retour à la ligne
+    printf("\t Apprendre a coder en C! ");	//permet de faire une tabulation au début
+
+    //Cette fonction retourne '0' si tout se passe bien
+    return 0;
+}
+
+
